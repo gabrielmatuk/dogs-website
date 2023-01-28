955b53fe1f4df23a9e0434f0229b382c1edc91bb
gabrielmatuk <gabrielmatuk14@gmail.com>
2023-01-27 21:11:52 -0300

    feat: finishing feed ui

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -5,22 +5,32 @@ import { PHOTOS_GET } from "../../constants";
 import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
 import styles from "./FeedPhotos.module.css";
+import { ReactComponent as Triste } from "../../Assets/cara-triste.svg";
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ page, user, setModalPhoto, setInifinite }) => {
   const { data, error, loading, request } = useFetch();
 
   React.useEffect(() => {
     const fetchPhotos = async () => {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { json } = await request(url, options);
-      console.log(json);
+      const total = 3;
+      const { url, options } = PHOTOS_GET({ page, total, user });
+      const { res, json } = await request(url, options);
+      if (res && res.ok && json.length < total) setInifinite(false);
     };
 
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInifinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
+  if (data && user && data.length === 0) {
+    return (
+      <div className={styles.nophoto}>
+        <Triste className={styles.imgSad} />
+        <p>Você ainda não tem fotos.</p>
+      </div>
+    );
+  }
   if (data)
     return (
       <ul className={`${styles.feed} animeLeft`}>
--- src/Components/Photo/PhotoDelete.js
+++ src/Components/Photo/PhotoDelete.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { PHOTO_DELETE } from "../../constants";
+import useFetch from "../../Hooks/useFetch";
 import styles from "./PhotoDelete.module.css";
 
 const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  const handleClick = async () => {
+    const confirm = window.confirm("Tem certeza que deseja deletar?");
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { res } = await request(url, options);
+      if (res.ok) window.location.reload();
+    }
+  };
+
   return (
-    <div>
-      <button className={styles.delete}>Deleter</button>
-    </div>
+    <>
+      {loading ? (
+        <button disabled className={styles.delete}>
+          Deleter
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}>
+          Deleter
+        </button>
+      )}
+    </>
   );
 };
 
